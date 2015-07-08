2f5c4321927d50e0513dea551f820e287875ff67
dhruvas <dhruvas@tavisca.com>
2015-07-08 17:54:10 +0530

    this is my first commit

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var firstMoneyObject = new Money();
+                var secondMoneyObject = new Money();
+                firstMoneyObject = Initialize(firstMoneyObject);
+                secondMoneyObject = Initialize(secondMoneyObject);
+                Money resultMoneyObject = firstMoneyObject + secondMoneyObject;
+                Console.WriteLine(resultMoneyObject.Amount + resultMoneyObject.currency);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static Money Initialize(Money moneyObject) 
+        {
+            Console.WriteLine("Enter the Amount");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Current");
+            string currency = Console.ReadLine();
+                moneyObject.Amount = amount;
+                moneyObject.currency = currency;
+                return moneyObject;
+        }
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Operator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OperatorOverloading.Model
+{
+    public class Money
+    {
+        private double _amount;
+        public double Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set 
+            {
+                if (value < 0 && value > double.MaxValue)
+                {
+                    throw new System.Exception(Resource.InvalidValue);
+                }
+                this._amount=value;
+            }
+        }
+        private string _currency;
+        public string currency
+        {
+            get
+            {
+                return _currency;
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(currency))
+                {
+
+                    throw new ArgumentNullException(Resource.InvalidCurrency);
+                }
+                else
+                {
+                    this._currency = value;
+                    
+
+                }
+                
+            }
+        }
+ 
+
+        public static Money operator +(Money firstMoneyObject, Money secondMoneyObject)
+        {
+            var thirdMoneyObject = new Money();
+            if (firstMoneyObject.currency.Equals(secondMoneyObject.currency,StringComparison.InvariantCultureIgnoreCase))
+            {
+                    thirdMoneyObject.Amount = firstMoneyObject.Amount + secondMoneyObject.Amount;
+                    thirdMoneyObject.currency = firstMoneyObject.currency;
+                    if (thirdMoneyObject.Amount > double.MaxValue)
+                    {
+                        throw new ArgumentException(Resource.InvalidSum);
+                    }
+                    
+            }
+            else
+            {
+                throw new System.Exception(Resource.InvalidCurrency);
+            }
+            return thirdMoneyObject;
+            
+        }
+    }
+}
