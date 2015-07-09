449ed2e2412d1684f579695164b9d88ceafa6368
dhruvas <dhruvas@tavisca.com>
2015-07-09 11:52:08 +0530

    final commit

--- 01 Operator Overloading/Operator Overloading.Host/Program.cs
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -1,45 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OperatorOverloading.Model;
 
 namespace OperatorOverloading.Host
 {
-    class Program
+    public class MainClass
     {
         public static void Main(string[] args)
         {
             try
             {
-                var firstMoneyObject = new Money(); 
-                var secondMoneyObject = new Money();
-                firstMoneyObject = Initialize(firstMoneyObject);
-                secondMoneyObject = Initialize(secondMoneyObject);
-                Money resultMoneyObject = firstMoneyObject + secondMoneyObject;
-                Console.WriteLine(resultMoneyObject.Amount + resultMoneyObject.currency);
+                Console.Write("Enter first Amount (Amount Currency) ");
+
+                Money amount1 = new Money(Console.ReadLine());
+
+                Console.Write("Enter second  Amount (Amount Currency) ");
+                Money amount2 = new Money(Console.ReadLine());
+
+                Console.Write(amount1 + amount2+" is the Total Amount  ");
+                
             }
+
             catch (Exception e)
             {
+                Console.WriteLine("Exception Occured.");
                 Console.WriteLine(e.Message);
             }
             Console.ReadLine();
-        }
-
-        private static Money Initialize(Money moneyObject)
-        {
-            double number;
-            Console.WriteLine("Enter the Amount");
-            if(double.TryParse(Console.ReadLine(),out number)==false)
-            {
-                throw new Exception(Resource1.InvalidAmount);
-            }
-            Console.WriteLine("Enter the Current");
-            string currency = Console.ReadLine();
-            moneyObject.Amount = number;
-            moneyObject.currency = currency;
-            return moneyObject;
         }
     }
 }
--- 01 Operator Overloading/Operator Overloading.Model/Operator.cs
+++ 01 Operator Overloading/Operator Overloading.Model/Operator.cs
@@ -1,79 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
 
 namespace OperatorOverloading.Model
 {
     public class Money
     {
         private double _amount;
+        private string _currency;
+
         public double Amount
         {
-            get
+            get { return _amount; }
+            private set
             {
-                return _amount;
-            }
-            set
-            {
-                if (value < 0 && value > double.MaxValue)
+                if (value < 0)
                 {
-                    throw new System.Exception();
+                    throw new Exception(Resources.InvalidAmount);
                 }
-                this._amount = value;
-            }
-        }
-        private string _currency;
-        public string currency
-        {
-            get
-            {
-                return _currency;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
+                if (double.IsPositiveInfinity(value))
                 {
-
-                    throw new ArgumentNullException(Resource1.InvalidCurrency);
+                    throw new Exception(Resources.InvalidAmount);
                 }
-                else
-                {
-                    this._currency = value;
-
-
-                }
-
+                _amount = value;
             }
         }
 
-
-        public static Money operator +(Money firstMoneyObject, Money secondMoneyObject)
+        public string Currency
         {
-            if (firstMoneyObject == null || secondMoneyObject == null)
+            get { return _currency; }
+            private set
             {
-                throw new Exception(Resource1.InvalidObject);
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception(Resources.InvalidCurrency);
+                }
+                _currency = value;
+            }
+        }
+
+        public Money(double amount, string currency)
+        {
+            Amount = amount;
+           Currency = currency;
+        }
+
+        public Money(string inputAmount)
+        {
+
+            if (string.IsNullOrWhiteSpace(inputAmount))
+            {
+                throw new Exception(Resources.InvalidAmount);
             }
 
-            var thirdMoneyObject = new Money();
-            if (firstMoneyObject.currency.Equals(secondMoneyObject.currency, StringComparison.InvariantCultureIgnoreCase))
-            {
-                thirdMoneyObject.Amount = firstMoneyObject.Amount + secondMoneyObject.Amount;
-                thirdMoneyObject.currency = firstMoneyObject.currency;
-                if (thirdMoneyObject.Amount > double.MaxValue)
-                {
-                    throw new ArgumentException(Resource1.InvalidSum);
-                }
+            var amountArr = inputAmount.Split(' ');
+            double amount;
 
+            if (amountArr.Length != 2)
+            {
+                throw new Exception(Resources.InvalidAmount);
+            }
+
+            if (double.TryParse(amountArr[0], out amount))
+            {
+                Amount = amount;
             }
             else
             {
-                throw new System.Exception(Resource1.InvalidCurrency);
+                throw new Exception(Resources.InvalidAmount);
             }
-            return thirdMoneyObject;
 
+            Currency = amountArr[1];
+        }
+
+        public static Money operator +(Money object1, Money object2)
+        {
+            if (object1 == null || object2 == null)
+            {
+                throw new Exception(Resources.InvalidObject);
+            }
+            if (object1.Currency.Equals(object2.Currency, StringComparison.CurrentCultureIgnoreCase))
+            {
+                double totalAmount = object1.Amount + object2.Amount;
+                return new Money(totalAmount, object1.Currency);
+            }
+            else
+            {
+                throw new Exception(Resources.InvalidCurrency);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Amount + " " + Currency;
         }
     }
 }
