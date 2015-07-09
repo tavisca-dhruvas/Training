28e42387f370efebee9522b49f5580aad077adeb
dhruvas <dhruvas@tavisca.com>
2015-07-09 15:37:16 +0530

    merge

--- 01 Operator Overloading/Operator Overloading.Host/Program.cs
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -10,11 +10,10 @@ namespace OperatorOverloading.Host
             try
             {
                 Console.Write("Enter first Amount (Amount Currency) ");
-
-                Money amount1 = new Money(Console.ReadLine());
+                var amount1 = new Money(Console.ReadLine());
 
                 Console.Write("Enter second  Amount (Amount Currency) ");
-                Money amount2 = new Money(Console.ReadLine());
+                var amount2 = new Money(Console.ReadLine());
 
                 Console.Write(amount1 + amount2+" is the Total Amount  ");
                 
--- 01 Operator Overloading/Operator Overloading.Model/Operator.cs
+++ 01 Operator Overloading/Operator Overloading.Model/Operator.cs
@@ -12,8 +12,8 @@ namespace OperatorOverloading.Model
             get { return _amount; }
             private set
             {
- 
-                if (double.IsPositiveInfinity(value) || value>double.MaxValue || value <0)
+
+                if (double.IsPositiveInfinity(value) || value > double.MaxValue || value < 0)
                 {
                     throw new Exception(Resources.InvalidAmount);
                 }
@@ -37,7 +37,7 @@ namespace OperatorOverloading.Model
         public Money(double amount, string currency)
         {
             Amount = amount;
-           Currency = currency;
+            Currency = currency;
         }
 
 
@@ -61,16 +61,12 @@ namespace OperatorOverloading.Model
                 throw new Exception(Resources.InvalidAmount);
             }
 
-            if (double.TryParse(amountArr[0], out amount))
-            {
-                Amount = amount;
-            }
-            else
+            if (double.TryParse(amountArr[0], out amount) == false)
             {
                 throw new Exception(Resources.InvalidAmount);
             }
-
-            Currency = amountArr[1];
+              Amount = amount;
+              Currency = amountArr[1];
         }
 
         public static Money operator +(Money object1, Money object2)
@@ -79,15 +75,16 @@ namespace OperatorOverloading.Model
             {
                 throw new Exception(Resources.InvalidObject);
             }
-            if (object1.Currency.Equals(object2.Currency, StringComparison.CurrentCultureIgnoreCase))
+            if (!object1.Currency.Equals(object2.Currency, StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new Exception(Resources.InvalidCurrency);
+            }
+            else
             {
                 double totalAmount = object1.Amount + object2.Amount;
                 return new Money(totalAmount, object1.Currency);
             }
-            else
-            {
-                throw new Exception(Resources.InvalidCurrency);
-            }
+
         }
 
         public override string ToString()
