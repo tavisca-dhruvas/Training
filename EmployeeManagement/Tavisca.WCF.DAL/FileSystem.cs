3c3b16958bbfab66ef99cef7b69816e0064368d7
dhruvas <dhruvas@tavisca.com>
2015-07-20 09:45:14 +0530

    second commmit

--- EmployeeManagement/Tavisca.WCF.DAL/FileSystem.cs
+++ EmployeeManagement/Tavisca.WCF.DAL/FileSystem.cs
@@ -5,47 +5,54 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using System.IO;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+
 namespace Tavisca.WCF.DAL
 {
     public class FileSystem
     {
         public void SaveEmployee(string jsonString, string id)
         {
-            if (File.Exists(@"D:\EmployeeID\ID.Txt"))
+            try
             {
-                File.AppendAllText(@"D:\EmployeeID\ID.Txt", id + Environment.NewLine);
+                if (File.Exists(@"D:\EmployeeDetails.Txt"))
+                    //throw new Exception("File doenot exists");
+               // else
+                    File.WriteAllText(@"D:\EmployeeDetails\" + id + ".Txt", jsonString);
             }
-            else
+            catch (Exception ex)
             {
-
-                File.WriteAllText(@"D:\EmployeeID\ID.Txt", id + Environment.NewLine);
+                bool rethrow = ExceptionPolicy.HandleException(ex, "Policy");
+                if (rethrow)
+                {
+                    throw;
+                }
             }
-
-            File.WriteAllText(@"D:\EmployeeDetails\" + id + ".Txt", jsonString);
-
-
         }
         public string RetrieveById(String id)
         {
-            var jsonString = File.ReadAllText(@"D:\EmployeeDetails\" + id );
-            return jsonString;
+                var jsonString = File.ReadAllText(@"D:\EmployeeDetails\" + id);
+                return jsonString;
+            
         }
 
         public List<string> RetrieveAllIds()
-       {
+        {
           
-          // var allId = File.ReadAllText(@"D:\EmployeeIDs\AllEmpID.Txt");
-           DirectoryInfo dir = new DirectoryInfo(@"D:\EmployeeDetails");
-           var files = dir.GetFiles("*.txt");
-            List<string> empId=new List<string>();
-            
-            
-           foreach (var file in files)
-           {
-            empId.Add(file.Name);   
-           }
-           return empId;
-          
-       }
+                // var allId = File.ReadAllText(@"D:\EmployeeIDs\AllEmpID.Txt");
+                DirectoryInfo dir = new DirectoryInfo(@"D:\EmployeeDetails");
+                var files = dir.GetFiles("*.txt");
+                List<string> empId = new List<string>();
+
+
+                foreach (var file in files)
+                {
+                    empId.Add(file.Name);
+                }
+                return empId;
+        }
+    
     }
 }
