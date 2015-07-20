154dd718cadafc96ccd1d01d7a08d203655fac08
dhruvas <dhruvas@tavisca.com>
2015-07-20 09:49:44 +0530

    first commit

---
+++ Bouncing Ball/js/bounce.js
@@ -0,0 +1,38 @@
+﻿var axisX = 0;
+var axisY = 0;
+var dx = 10;
+var dy = 10;
+
+function initialize() {
+    setInterval(bounceBall, 20);
+
+}
+function bounceBall() {
+    var height = (window.innerHeight - 70);
+    var width = (window.innerWidth - 70);
+
+    axisX += dx;
+    axisY += dy;
+    var element = document.getElementById('ball');
+    element.style.left = axisX + "px";
+    element.style.top = axisY + "px";
+
+
+    if (axisY >= height) {
+        dy = -10;
+        return;
+    }
+    if (axisX >= width) {
+        dx = -10;
+        return;
+    }
+    if (axisY <= 0) {
+        dy = 10;
+        return;
+    }
+    if (axisX <= 0) {
+        dx = 10;
+        return;
+    }
+
+}
