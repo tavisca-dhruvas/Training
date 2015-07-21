6fee0b08ce1518ab4e56d306e2ae92011ac09ee9
dhruvas <dhruvas@tavisca.com>
2015-07-21 16:59:52 +0530

    second commit for Tic-Tac-Toe

--- TicTacToe/js/index.js
+++ TicTacToe/js/index.js
@@ -2,8 +2,8 @@
 var selectedPlayer;
 var chanceCount;
 function MarkerClicked(selectedMarker) {
-	document.getElementById("start-button").disabled=false;
-    document.getElementById("start-button").style.opacity=1;
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("start-button").style.opacity = 1;
     document.getElementById("tick").style.visibility = "visible";
     if (selectedMarker === 1) {
         document.getElementById("selected").style.marginLeft = "70px";
@@ -19,13 +19,12 @@ function StartGame() {
     var i, j;
     document.getElementById("first-screen").style.display = "none";
     document.getElementById("game-board").style.display = "block";
-    document.getElementById("game-board").s
-    for (i=0;i<3;i++) {
-       document.getElementById("game-board").innerHTML += "<div>";
-       for (j=0;j<3;j++) {
-          document.getElementById("game-board").innerHTML += "<input onclick='MarkerPlaced(event)' type='button' id='block"+i+j+"'/>";
-       }
-       document.getElementById("game-board").innerHTML += "</div>";
+    for (i = 0; i < 3; i++) {
+        document.getElementById("game-board").innerHTML += "<div>";
+        for (j = 0; j < 3; j++) {
+            document.getElementById("game-board").innerHTML += "<input onclick='MarkerPlaced(event)' type='button' id='block" + i + j + "'/>";
+        }
+        document.getElementById("game-board").innerHTML += "</div>";
     }
 }
 
@@ -37,32 +36,30 @@ function MarkerPlaced(event) {
     document.getElementById(block).value = selectedPlayer;
     document.getElementById(block).disabled = true;
     chanceCount++;
-    if(selectedPlayer == 1) {
-       document.getElementById(block).style.backgroundImage = "url('../images/x.png')";
-       selectedPlayer = 2;
+    if (selectedPlayer == 1) {
+        document.getElementById(block).style.backgroundImage = "url('../images/x.png')";
+        selectedPlayer = 2;
     } else {
-       document.getElementById(block).style.backgroundImage = "url('../images/o.png')";
-       selectedPlayer = 1;
+        document.getElementById(block).style.backgroundImage = "url('../images/o.png')";
+        selectedPlayer = 1;
     }
-    if(chanceCount==9)
-    {
-        if(checkWinningConditions(document.getElementById(block).value))
-        	setTimeout(function(){showResult(document.getElementById(block).value);},400);
+    if (chanceCount == 9) {
+        if (checkWinningConditions(document.getElementById(block).value))
+            setTimeout(function () { showResult(document.getElementById(block).value); }, 400);
         else
-        	setTimeout(function(){showResult('draw');},400);
+            setTimeout(function () { showResult('draw'); }, 400);
     }
-    if(chanceCount>4)
-    {
-        if(checkWinningConditions(document.getElementById(block).value))
-        	setTimeout(function(){showResult(document.getElementById(block).value);},400);
+    if (chanceCount > 4) {
+        if (checkWinningConditions(document.getElementById(block).value))
+            setTimeout(function () { showResult(document.getElementById(block).value); }, 400);
     }
-    //if(count)
+  
 }
 
-function StartAgain(){
-	document.getElementById("result-container").style.display = "none";
+function StartAgain() {
+    document.getElementById("result-container").style.display = "none";
     document.getElementById("first-screen").style.display = "block";
     document.getElementById("tick").style.visibility = "hidden";
-    document.getElementById("start-button").disabled=true;
-    document.getElementById("start-button").style.opacity=.5;
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("start-button").style.opacity = .5;
 }
