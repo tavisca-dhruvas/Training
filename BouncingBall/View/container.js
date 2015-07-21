ff1068c25c104fe70f9a03e6534d146f895d32a5
dhruvas <dhruvas@tavisca.com>
2015-07-21 10:29:20 +0530

    first commit with changes Bouncing ball

---
+++ BouncingBall/View/container.js
@@ -0,0 +1,25 @@
+﻿window.ball = window.ball || {}
+
+window.ball.boundaryCondition = function () {
+
+    var height = window.innerHeight - 200;
+    var width = window.innerWidth - 200;
+    if (window.ball.xPosition < 0) {
+        window.ball.xDirection = true;
+        window.ball.xPosition = 0;
+    }
+    if (window.ball.xPosition >= (width)) {
+        window.ball.xDirection = false;
+        window.ball.xPosition = (width);
+    }
+
+
+    if (window.ball.yPosition < 0) {
+        window.ball.yDirection = true;
+        window.ball.yPosition = 0;
+    }
+    if (window.ball.yPosition >= height) {
+        window.ball.yDirection = false;
+        window.ball.yPosition = height;
+    }
+}
