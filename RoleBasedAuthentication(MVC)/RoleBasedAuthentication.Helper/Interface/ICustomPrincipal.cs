8b79cfbb2ee5811029766a94a882077f5fa0fae9
dhruvas <dhruvas@tavisca.com>
2015-08-12 18:21:32 +0530

    First Commit for Mvc

---
+++ RoleBasedAuthentication(MVC)/RoleBasedAuthentication.Helper/Interface/ICustomPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleBasedAuthentication.Helper.Interface
+{
+    interface ICustomPrincipal : System.Security.Principal.IPrincipal
+    {
+        string EmpId { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string Title { get; set; }
+
+    }
+}
